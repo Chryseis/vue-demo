e6ac6ae78668950d86724736b110d905d8daa4f4
chrys <294931812@qq.com>
2020-05-21 11:42:41 +0800

    add code

--- src/pages/list/index.vue
+++ src/pages/list/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>{{ menu.title }}{{ menu.content }}</div>
+  <div>
+    <el-button type="primary" @click="open">点我</el-button>
+    <el-button type="primary" @click="close">buttonCont</el-button>
+  </div>
 </template>
 
 <script>
+import { MessageBox, Message } from 'element-ui';
+
 export default {
   name: 'index',
   data() {
@@ -15,6 +20,31 @@ export default {
   },
   created() {
     console.log(this.menu);
+  },
+  methods: {
+    open() {
+      MessageBox.confirm('modal1 content', 'modal1', {
+        center: true,
+        confirmButtonText: '好',
+        cancelButtonText: '不',
+        beforeClose(action, instance, done) {
+          switch (action) {
+            case 'confirm':
+              MessageBox.close();
+              Message.info('hahaha');
+              break;
+            case 'cancel':
+            case 'close':
+              MessageBox.close();
+              setTimeout(() => MessageBox.alert('hahah', 'modal1'), 300);
+              break;
+          }
+        }
+      });
+    },
+    close() {
+      MessageBox.close();
+    }
   }
 };
 </script>
