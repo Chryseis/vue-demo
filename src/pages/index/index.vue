4994ee82a79ab1cabba7ea57ad5535968e68a77a
allen <294931812@qq.com>
2021-06-29 15:32:53 +0800

    add code

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -9,6 +9,8 @@
         {{ route.name }}
       </el-button>
     </section>
+    <img src="@/assets/imgs/img1.png" alt="" @load="imgLoad" />
+    <img :src="data.src" alt="" @load="imgLoad" />
     <section class="person-info">
       <div class="label">name:{{ data.name }}</div>
       <div class="label">sex:{{ data.sex }}</div>
@@ -29,17 +31,12 @@ export default {
     }
   },
   mounted() {
-    let i = 0
-
-    while (i < 100000) {
-      i = i + 1
-    }
-
     this.mockRequest(
       {
         name: 'allen',
         sex: 'male',
-        major: 'engineer'
+        major: 'engineer',
+        src: require('@/assets/imgs/img2.png')
       },
       300
     ).then(data => {
@@ -56,15 +53,16 @@ export default {
       })
     },
     computePerformance() {
-      const roundByFour = (num, digits = 4) => {
-        try {
-          return parseFloat(num.toFixed(digits))
-        } catch (e) {
+      try {
+        const roundByFour = (num, digits = 4) => {
+          if (num) {
+            return parseFloat(num.toFixed(digits))
+          }
           return 0
         }
-      }
 
-      window.addEventListener('load', () => {
+        let ccp
+
         const navigationTiming =
           performance.getEntriesByType('navigation').length > 0
             ? performance.getEntriesByType('navigation')[0]
@@ -75,12 +73,21 @@ export default {
         const tcp = navigationTiming.connectEnd - navigationTiming.connectStart
         const html = navigationTiming.responseEnd - navigationTiming.requestStart
         const domParse = navigationTiming.domInteractive - navigationTiming.responseEnd
-        const ccp = performance.now()
         const fcp = performance.getEntriesByType('paint').find(entry => entry.name === 'first-contentful-paint')
           ?.startTime
 
         const jsArr = resource.filter(r => /^.+\.js(?:\?.+|)$/.test(r.name))
         const cssArr = resource.filter(r => /^.+\.css(?:\?.+|)$/.test(r.name))
+        const images = document.querySelectorAll('img')
+
+        images.forEach(image => {
+          image.onload = () => {
+            ccp = performance.now()
+          }
+          image.onerror = err => {
+            console.log('image err', err)
+          }
+        })
 
         const jsLoadDuration =
           jsArr.length > 0
@@ -92,44 +99,65 @@ export default {
             ? Math.max(...cssArr.map(ele => ele.responseEnd)) - Math.min(...cssArr.map(ele => ele.fetchStart))
             : 0
 
+        const stopEvent = ['click', 'touchstart']
+
+        const timeLimit = 5000
+
+        let timerId
+
+        let lcp = 0
+
+        let po
+
         if (!!PerformanceObserver && PerformanceObserver.supportedEntryTypes.includes('largest-contentful-paint')) {
-          const po = new PerformanceObserver(l => {
-            l.getEntries().map(entry => {
+          po = new PerformanceObserver(l => {
+            l.getEntries().forEach(entry => {
               if (entry.startTime) {
-                __bl.performance({
-                  cfpt: roundByFour(fcp),
-                  ctti: roundByFour(ccp),
-                  t1: roundByFour(dns),
-                  t2: roundByFour(tcp),
-                  t3: roundByFour(navigationTiming.requestStart),
-                  t4: roundByFour(navigationTiming.responseEnd),
-                  t5: roundByFour(html),
-                  t6: roundByFour(entry.startTime),
-                  t7: roundByFour(domParse),
-                  t8: roundByFour(jsLoadDuration),
-                  t9: roundByFour(cssLoadDuration)
-                })
+                lcp = roundByFour(entry.startTime)
               }
             })
           })
 
           po.observe({ type: 'largest-contentful-paint', buffered: true })
-        } else {
+        }
+
+        const handleEvent = () => {
+          timerId && clearTimeout(timerId)
+          po && po.disconnect()
+          ccp = Math.min(timeLimit, ccp)
+
           __bl.performance({
-            cfpt: roundByFour(fcp),
+            cfpt: fcp,
             ctti: roundByFour(ccp),
             t1: roundByFour(dns),
             t2: roundByFour(tcp),
-            t3: roundByFour(navigationTiming.requestStart),
-            t4: roundByFour(navigationTiming.responseEnd),
+            t3: roundByFour(navigationTiming.responseStart - navigationTiming.requestStart),
+            t4: roundByFour(navigationTiming.responseEnd - navigationTiming.responseStart),
             t5: roundByFour(html),
-            t6: 0,
+            t6: lcp,
             t7: roundByFour(domParse),
             t8: roundByFour(jsLoadDuration),
             t9: roundByFour(cssLoadDuration)
           })
+
+          stopEvent.forEach(event => {
+            document.body.removeEventListener(event, handleEvent)
+          })
         }
-      })
+
+        stopEvent.forEach(event => {
+          document.body.addEventListener(event, handleEvent, { once: true })
+        })
+
+        timerId = setTimeout(() => {
+          handleEvent()
+        }, timeLimit)
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    imgLoad(e) {
+      console.log('img load', e, performance.now())
     }
   }
 }
