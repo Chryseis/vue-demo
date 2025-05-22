1fe49798bd4467e56f7ce1052e25ecdd2735278a
allen <294931812@qq.com>
2025-05-22 18:27:04 +0800

    add code

--- src/pages/genPic/index.vue
+++ src/pages/genPic/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-button type="primary" @click="genQR">生成图片</el-button>
+    <el-button type="primary" @click="genMini">生成图片</el-button>
     <div id="container"></div>
   </div>
 </template>
@@ -9,6 +9,69 @@
 import Konva from 'konva'
 import qrcode from 'qrcode'
 import img from '@/assets/imgs/img.png'
+import bg from '@/assets/imgs/bg.png'
+import miniCode from '@/assets/imgs/mini-code.png'
+
+const createLayer = ({ img, container }) => {
+  return new Promise((resolve, reject) => {
+    const imageObj = new Image()
+    imageObj.onload = e => {
+      const { naturalWidth, naturalHeight } = e.target
+      const stage = new Konva.Stage({
+        container,
+        width: naturalWidth,
+        height: naturalHeight
+      })
+      const layer = new Konva.Layer()
+
+      stage.add(layer)
+
+      stage.on('mousemove', () => {
+        const pos = stage.getPointerPosition()
+        console.log(`x: ${pos.x}, y: ${pos.y}`)
+      })
+
+      resolve(layer)
+    }
+    imageObj.src = img
+  })
+}
+
+const drawImage = ({ img, x = 0, y = 0, width, height }) => {
+  return new Promise((resolve, reject) => {
+    const imageObj = new Image()
+    imageObj.onload = e => {
+      const { naturalWidth, naturalHeight } = e.target
+
+      const image = new Konva.Image({
+        x,
+        y,
+        image: imageObj,
+        width: width || naturalWidth,
+        height: height || naturalHeight
+      })
+
+      resolve(image)
+    }
+    imageObj.src = img
+  })
+}
+
+const drawText = ({ text, x, y, color }) => {
+  return new Konva.Text({
+    x,
+    y,
+    text,
+    fontSize: 60,
+    width: 700, // 设置宽度以启用换行
+    fill: color,
+    padding: 20,
+    align: 'center',
+    fontFamily: 'Calibri',
+    lineHeight: 1.3
+  })
+}
+
 export default {
   name: 'genPic',
   methods: {
@@ -60,6 +123,20 @@ export default {
           imageObj.src = img
         }
       )
+    },
+    async genMini() {
+      const layer = await createLayer({ img: bg, container: 'container' })
+
+      const image = await drawImage({ img: bg })
+
+      const mini = await drawImage({ img: miniCode, x: 300, y: 730, width: 520, height: 520 })
+
+      const text = drawText({ text: '武汉太康智云测试一二三四五六七八九大药房', x: 221, y: 430, color: '#fff' })
+
+      layer.add(image)
+      layer.add(mini)
+      layer.add(text)
+      layer.draw()
     }
   }
 }
